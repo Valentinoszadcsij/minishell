3b5f8c08de6a4b11d953fa9b67d659434a74821d
Valentin Oszadcsij <voszadcs@2-A-6.42heilbronn.de>
2023-08-16 22:40:48 +0200

    basic tokenizer done

--- src/lexer.c
+++ src/lexer.c
@@ -6,7 +6,7 @@
 /*   By: voszadcs <voszadcs@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/11 20:45:49 by voszadcs          #+#    #+#             */
-/*   Updated: 2023/08/15 20:12:56 by voszadcs         ###   ########.fr       */
+/*   Updated: 2023/08/16 22:00:17 by voszadcs         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,6 @@ int	tokenize(char *str, t_mylist *list)
 	i = 0;
 	while (str[i] == ' ')
 		i++;
-	start = i;
 	if (str[i] == '<' && str[i + 1] != '<')
 		return (list->type = LS, list->value = NULL, i + 1);
 	else if (str[i] == '<' && str[i + 1] == '<')
@@ -32,42 +31,50 @@ int	tokenize(char *str, t_mylist *list)
 		return (list->type = PIPE, list->value = NULL, i + 1);
 	else
 	{
+		start = i;
+		if (str[i] == '\0')
+			return (list->type = -1, i);
 		while (str[i] != '\0' && str[i] != ' ')
 			i++;
-		return (list->type = WRD, list->value = ft_substr(str, start, i - start), i + 1);
+		return (list->type = WRD, list->value = ft_substr(str, start, i - start), i);
 	}
+}
+
+void	fill_list(t_mylist	**list, char *str, int *index)
+{
+	t_mylist	*node;
+	t_mylist	*head;
+	int			i;
+
+	i = 0;
+		node = malloc (sizeof(t_mylist));
+	if (node == NULL)
+		exit(0);//_error(ERR_MALLOC);
+	node->next = NULL;
+	i = tokenize(&str[*index], node);
+	if (*list == NULL)
+		*list = node;
+	else
+	{
+		head = *list;
+		while (head->next != NULL)
+			head = head->next;
+		if (node->type != -1)
+			head->next = node;
+	}
+	*index = *index + i;
 }
 
 t_mylist	*lexer(char *str)
 {
 	t_mylist	*list;
-	t_mylist	*node;
-	t_mylist	*head;
 	char		*current;
-	int			i;
 	int			index;
 
 	list = NULL;
 	current = str;
 	index = 0;
-	i = 0;
 	while (current[index] != '\0')
-	{
-		node = malloc (sizeof(t_mylist));
-		if (node == NULL)
-			exit(0);//_error(ERR_MALLOC);
-		i = tokenize(&current[index], node);
-		if (list == NULL)
-			list = node;
-		else
-		{
-			head = list;
-			while (head->next != NULL)
-				head = head->next;
-			head->next = node;
-		}
-		index = index + i;
-		printf("Index %d\n", index);
-	}
+		fill_list(&list, current, &index);
 	return (list);
 }
